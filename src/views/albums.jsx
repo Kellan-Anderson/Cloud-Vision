939d567f121d4cffc1d09ef75e51b99bb24c65d5
Collins Wyatt <collinsleewyatt@gmail.com>
2023-04-24 17:44:45 -0400

    We now show the user's albums!

--- src/views/albums.jsx
+++ src/views/albums.jsx
@@ -5,6 +5,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import Loading from '../components/Loading';
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 // import {SignIn} from "../components/signin"
 
@@ -12,28 +13,34 @@ export default function Albums() {
   const getFirebaseApp = useContext(FirebaseContext);
   const app = getFirebaseApp();
   const db = getFirestore(app);
-
+  const storage = getStorage();
   const auth = getAuth(app);
   const [authUser, loading, error] = useAuthState(auth);
   const [user, setUser] = useState(authUser);
-
   useEffect(() => {
     setUser(authUser);
   }, [authUser, loading]);
 
+  const [urls, setUrls] = useState(null);
+
   let [result, firestoreLoading, firestoreError] = useCollection(
     user ? collection(getFirestore(app), user.uid) : null
   )
+  if(result && urls === null) {
+    Promise.all(result?.docs.map((doc) => getDownloadURL(ref(storage, doc.data().uri)))).then((urlArray) => {
+      setUrls(urlArray);
+    })
+  }
 
   return (
     <>
       {loading ? <Loading /> :
         <>
           {user ?
-            <>
-              {result?.docs.map((doc) => <p key={doc.id}>{doc.data().uri}</p> )}
+            <div className="grid grid-cols-3">
+              {urls ? urls.map((url, index) => <img draggable="true" className="w-64 h-64 object-scale-down" key={index} src={url}></img>) : <p></p>}
               {/* Write album component here */}
-            </>
+            </div>
             :
             <div className="flex flex-col justify-center items-center">
               <h1 className="mb-8">Please sign in to view this page</h1>
