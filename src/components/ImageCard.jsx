f6e44607c71d7bce940c0f5e781a71d51daa90c5
Collins Wyatt <collinsleewyatt@gmail.com>
2023-04-25 23:13:57 -0400

    adding a silly little hover effect

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -11,7 +11,7 @@ export default function ImageCard({ imageDoc }) {
     <a
       href={`/images/${imageDoc.id}`} 
       className="border-stone-950 border-2 flex flex-col justify-center items-center w-72 m-6 px-3 
-                  py-3 rounded-xl bg-stone-800 justify-self-center"
+                  py-3 rounded-xl bg-stone-800 justify-self-center hover:shadow-md hover:scale-110 transition-all duration-200"
     >
       <img src={url} alt={filename} className="h-48 w-fit"/>
       <h2 id="card-title" className="break-words text-center my-1 font-semibold">{filename}</h2>
