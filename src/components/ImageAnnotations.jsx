fe403c422ceb2344ff7999fb05ef37ab46a78f84
Collins Wyatt <collinsleewyatt@gmail.com>
2023-04-26 10:44:16 -0400

    object annotations take up space and margin

--- src/components/ImageAnnotations.jsx
+++ src/components/ImageAnnotations.jsx
@@ -6,39 +6,39 @@ export default function ImageAnnotations({doc, url}) {
     const parentDiv = useRef(null);
     const image = useRef(null);
 
-    const vertices = doc.data().object_detection.localizedObjectAnnotations[0].boundingPoly.normalizedVertices;
+    const vertices = doc.data().object_detection?.localizedObjectAnnotations?.[0]?.boundingPoly?.normalizedVertices;
 
-    return <div className="relative h-96 w-96 mt-16">
-        <div ref={parentDiv} className="object-scale-down">
-            <img ref={image} onLoad={() => setCoords([image.current.width, image.current.height])} className="absolute" src={url} alt={doc.id} />
-            {
-            imageLoaded ?
-                <svg className="absolute" viewBox={`0 0 ${x} ${y}`}>
-                    {/** see https://stackoverflow.com/questions/15500894/background-color-of-text-in-svg */}
-                      <defs>
-                        <filter x="0" y="0" width="1" height="1" id="solid">
-                        <feFlood flood-color="blue" result="bg" />
-                        <feMerge>
-                            <feMergeNode in="bg"/>
-                            <feMergeNode in="SourceGraphic"/>
-                        </feMerge>
-                        </filter>
-                    </defs>
-                    {/** From all annotations, we need to get  */}
-                     {doc.data().object_detection.localizedObjectAnnotations.map((annotation, index) => {
-                        const normalized = normalize(annotation.boundingPoly.normalizedVertices, x, y)
-                        return <>
-                        <text filter="url(#solid)" x={normalized[1].x} y={normalized[1].y} fill="blue" textAnchor="end" fontSize={x / 25}>{annotation.name}</text>
-                        <path key={index} d={pathString(normalized)} fill="none" stroke="blue" stroke-width="5" />
-                        </>
-                     }
-                      )}
-                </svg>
-            : <></>
-            }
-        </div>
+    return <div className="relative flex w-3/5 h-3/5 mt-16">
+    <div ref={parentDiv} className="relative w-full h-full">
+      <img ref={image} onLoad={() => setCoords([image.current.width, image.current.height])} className="" src={url} alt={doc.id} />
+      {imageLoaded ? (
+        <svg className="absolute inset-0 w-full h-full" viewBox={`0 0 ${x} ${y}`}>
+          {/** see https://stackoverflow.com/questions/15500894/background-color-of-text-in-svg */}
+          <defs>
+            <filter x="0" y="0" width="1" height="1" id="solid">
+              <feFlood flood-color="blue" result="bg" />
+              <feMerge>
+                <feMergeNode in="bg" />
+                <feMergeNode in="SourceGraphic" />
+              </feMerge>
+            </filter>
+          </defs>
+          {/** From all annotations, we need to get  */}
+          {doc.data().object_detection?.localizedObjectAnnotations?.map((annotation, index) => {
+            const normalized = normalize(annotation.boundingPoly.normalizedVertices, x, y);
+            return (
+              <>
+                <text filter="url(#solid)" x={normalized[1].x} y={normalized[1].y} fill="blue" textAnchor="end" fontSize={x / 25}>
+                  {annotation.name}
+                </text>
+                <path key={index} d={pathString(normalized)} fill="none" stroke="blue" strokeWidth="5" />
+              </>
+            );
+          })}
+        </svg>
+      ) : <></>}
     </div>
-
+  </div>
 }
 
 // convert normalized coordinates (0 to 1 representing percentage of full image) to a string representing
